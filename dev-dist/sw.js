c6f8ffa6ba55f44ee9df346a9f651e8905a1df05
agarcia <angelgarciap.dev@gmail.com>
2026-06-23 17:43:45 -0600

    agregar fallback

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b4l3m12evvo"
+    "revision": "0.3up837si23s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
